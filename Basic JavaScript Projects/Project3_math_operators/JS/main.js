cb76556443d2bce8e6156489aeb13eafca217677
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-05-19 07:04:12 +0100

    JS Math operators and JS functions

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -68,8 +68,8 @@ function circleArea() {
     ).innerHTML = `This small program uses three Math methods: <br /> 
     <strong>Math.pow(r,2) - to get the second power of the radius
     <br />Math.PI - because you need the PI number to count the area of the circle,
-    <br />Math.roun() - just to round the result. </strong><br /><br />
-    It also check if the input provided by user is a valid (numberic) input.<br />
+    <br />Math.round() - just to round the result. </strong><br /><br />
+    It also check if the input provided by user is a valid (numeric) input.<br />
     - If user provided number - the function will parse the number from the input and provide the result (Even if the input is mixted with NaN f.e. 2cm).<br />
     - If user provided invalid input NaN, the function will prompt the alert message. 
     `;
